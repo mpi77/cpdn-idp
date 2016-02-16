ab51eabe4cec55808e618a21ba112f795e88ddaf
Martin Bureš <m2bures@gmail.com>
2016-02-16 20:44:36 +0100

    added notfound plugin and controllerbase

---
+++ app/controllers/ControllerBase.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace CpdnIDP\Controllers;
+
+use Phalcon\Mvc\Controller;
+
+class ControllerBase extends Controller {
+}
---
+++ app/plugins/NotFoundPlugin.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace CpdnIDP\Plugins;
+
+use Phalcon\Events\Event;
+use Phalcon\Mvc\User\Plugin;
+use Phalcon\Dispatcher;
+use Phalcon\Mvc\Dispatcher\Exception as DispatcherException;
+use Phalcon\Mvc\Dispatcher as MvcDispatcher;
+use \Exception;
+
+/**
+ * NotFoundPlugin
+ *
+ * Handles not-found controller/actions
+ */
+class NotFoundPlugin extends Plugin {
+	
+	/**
+	 * This action is executed before execute any action in the application
+	 *
+	 * @param Event $event        	
+	 * @param Dispatcher $dispatcher        	
+	 */
+	public function beforeException(Event $event, MvcDispatcher $dispatcher, Exception $exception) {
+		if ($exception instanceof DispatcherException) {
+			switch ($exception->getCode ()) {
+				case Dispatcher::EXCEPTION_HANDLER_NOT_FOUND :
+				case Dispatcher::EXCEPTION_ACTION_NOT_FOUND :
+					$dispatcher->forward ( array (
+							'controller' => 'errors',
+							'action' => 'e404' 
+					) );
+					return false;
+			}
+		}
+		
+		  echo "<pre>";
+		  var_dump($exception);
+		  echo "</pre>";
+		  exit();
+		 
+		$dispatcher->forward ( array (
+				'controller' => 'errors',
+				'action' => 'e500' 
+		) );
+		return false;
+	}
+}
